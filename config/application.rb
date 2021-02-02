73514806853b386f966b48a33efcb6f2d2e22bcb
Renato Marques <renatomarques.contato@gmail.com>
2021-02-02 16:59:20 +0000

    Controller session e tela de login

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module ContactList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -15,5 +17,6 @@ module ContactList
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale= :'pt-BR'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
 
   resources :users, only: [:new, :create]
+
+  get 'entrar' , to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
 end
