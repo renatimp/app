b63f2b2960572df8bd8896d7da1cda6c90d594d5
Renato Marques <renatomarques.contato@gmail.com>
2021-02-02 16:28:00 +0000

    Ajustando formulario de signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   def new
-    # code
+    @user = User.new
   end
 
   def create
-    # code
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Usuário cadastrado com sucesso'
+      redirect_to root_url
+    else
+      render 'new'
+
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
 end
